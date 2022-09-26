7c02cc63ceae5bf9112ce23c29fad4cbb5dd9b79
Benrise <benrise_x@mail.ru>
2022-09-26 15:07:03 +0300

    First commit

---
+++ fn.py
@@ -0,0 +1,21 @@
+import os
+import pynput
+import playsound
+from pynput import keyboard
+from playsound import playsound
+
+def on_press(key):
+    key_str = '{0}'.format(key)
+    if (key_str == '<179>'):
+        stream = os.popen('/usr/local/bin/issw')
+        output = stream.read().strip()
+        if (output == 'com.apple.keylayout.ABC'):
+            os.system('com.apple.keylayout.Russian')
+            playsound('/Users/benristar/knopka-klik-zvonkii-myagkaya.wav')
+        else:
+            os.system('com.apple.keylayout.ABC')
+            playsound('/Users/benristar/knopka-klik-zvonkii-myagkaya.wav')
+
+
+with keyboard.Listener(on_press=on_press, on_release=None) as listener:
+    listener.join()
